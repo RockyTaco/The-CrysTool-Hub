845b07abf3ad34bf28bb1b0a4a22549c5652105f
RockyTaco <115428223+RockyTaco@users.noreply.github.com>
2024-08-09 16:07:15 -0400

    Fixing for vercel

--- src/pages/document.js
+++ src/pages/document.js
@@ -1,17 +1,20 @@
 // pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-import { Html, Head, Main, NextScript } from 'next/document';
-
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Add any custom head elements here */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
